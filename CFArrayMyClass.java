2283d9ddf4ff6749d925abbb59e85e5778cac84e
Srijit Bhattacharya <56132120+srijit43@users.noreply.github.com>
2024-10-22 10:55:34 -0700

    Add files via upload
    Initialize an array and iterate it, print it

---
+++ CFArrayMyClass.java
@@ -0,0 +1,38 @@
+/*
+Welcome to JDoodle!
+
+You can execute code here in 88 languages. Right now you’re in the Java IDE.
+
+  1. Click the orange Execute button ▶ to execute the sample code below and see how it works.
+
+  2. Want help writing or debugging code? Type a query into JDroid on the right hand side ---------------->
+
+  3.Try the menu buttons on the left. Save your file, share code with friends and open saved projects.
+
+Want to change languages? Try the search bar up the top.
+*/
+
+public class MyClass {
+  public static void main(String args[]) {
+    int[] riko = new int[3]; // declare an array
+        
+    riko[0] = 1;
+    riko[1] = 2;
+    riko[2] = 3;
+        
+    for(int i=0;i<riko.length;i++){
+        System.out.println(riko[i]);
+         
+    }
+        
+        
+    String[] bhatta = {"apple", "oranges", "banana", "peaches"};
+    
+    for(int j=0;j<bhatta.length;j++){
+        System.out.println(bhatta[j]);
+    }
+    
+        
+        
+  }
+}
